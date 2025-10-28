a2d2a30f1934514f3289d8d4237e1d241c14b1aa
Santhiago Dionizio Pinto <santhiagosdp@gmail.com>
2025-10-28 19:42:11 -0300

    ref

--- fiado_pro/urls.py
+++ fiado_pro/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include(('carteira.urls', 'carteira'), namespace='carteira')),
+    #reset senha por e-mail
+    path("accounts/password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("accounts/password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("accounts/reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("accounts/reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
 ]
