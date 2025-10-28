abbcd19a2ee5e2056ff5ef44a9440356c66eada2
Santhiago Dionizio Pinto <santhiagosdp@gmail.com>
2025-10-28 19:45:12 -0300

    f

--- fiado_pro/urls.py
+++ fiado_pro/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from fiado_pro.views_auth import SignUpView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
